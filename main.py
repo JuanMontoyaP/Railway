13ef9bebd418d55b376599101dd9d432d2ced576
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-04 17:28:50 -0500

    Add: Add the function read_data to main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-
+from read import read_data
 
 def main():
-    # TODO Read the files
+    # Read the files
+    rail_roughness = read_data()
     # TODO Calculate the growth factor
     # TODO Calculate when the rail is going to fail
     # TODO Tell the user when the rail is going to fail
