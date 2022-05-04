0953893d4db2e4dcff33d9f7414964b5ce2bba6d
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-04 15:38:39 -0500

    Add: main file created

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    # TODO Read the files
+    # TODO Calculate the growth factor
+    # TODO Calculate when the rail is going to fail
+    # TODO Tell the user when the rail is going to fail
+    # TODO Show the graphs with the three norms 
+    pass
+
+if __name__ == "__main__":
+    main()
