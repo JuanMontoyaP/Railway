47cf6e5e7ba6ce3a3cbc8e835f8039a6ff8867d6
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-06 23:47:22 -0500

    Add: Move inser_data file to the datahandling package

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import userinterface.ui as ui
-import insert_data as id
+# import insert_data as id
 
 def main():
     # selection = ui.main_page()
     # rail_roughness = read_data()
-    id.insert_rail_data()
+    # id.insert_rail_data()
     # TODO Calculate the growth factor
     # TODO Calculate when the rail is going to fail
     # TODO Tell the user when the rail is going to fail
