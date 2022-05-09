8b2ae633194f9e63171fa88f9fe30655fb3aac97
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-08 19:45:32 -0500

    Add: The logic for the different pages

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import sys
+
 from pages import mainpage as mp
 
 def main():
     selection = mp.main_page()
+    
+    if selection == 1:
+        pass
+    elif selection == 2:
+        pass
+    else:
+        sys.exit("Exit program")
+    
     # rail_roughness = read_data()
+
     # TODO Calculate the growth factor
     # TODO Calculate when the rail is going to fail
     # TODO Tell the user when the rail is going to fail
