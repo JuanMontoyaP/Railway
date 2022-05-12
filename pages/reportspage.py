04a471666826c96c95456f4daac293fa3b3bf904
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-11 23:25:21 -0500

    Add: The generate reports page

--- pages/reportspage.py
+++ pages/reportspage.py
@@ -14,12 +14,12 @@ def print_insert_main_page():
     """
     page = "Reports Page"
     description = "This page manages the reports"
-    options = ["Actual state", "Make a prediction", "Select a certain curve"]
+    options = ["Generate reports", "Make a prediction"]
     zero_value = "Exit"
     ui.print_page(page, description, options, zero_value)
     return ui.page_selection(len(options))
 
-def generates_make_predictions_options() -> List:
+def unique_curves(keys) -> List:
     """
     It returns a list of strings that are formatted as "Curve X, Y" where X is the curve number and Y is
     the thread number
@@ -27,13 +27,14 @@ def generates_make_predictions_options() -> List:
     - Return: 
         - A list of strings.
     """
-    records = unique_items_in_nested_list(data_available(["curve", "thread"]))
-    return [f'Curve {record[0]}, {record[1]}' for record in records]
+    return unique_items_in_nested_list(data_available(keys))
 
 def print_make_predictions_page():
     page = "Predictions Page"
     description = "This page predicts when the rail is going to fail"
-    options = generates_make_predictions_options()
+    # options = unique_curves()
+    options = [f'Curve {record[0]}, {record[1]}' for record in unique_curves(["curve", "thread"])]
+
     ui.print_page(page, description, options, "Exit")
     selection = ui.page_selection(len(options))
     if selection:
@@ -41,14 +42,24 @@ def print_make_predictions_page():
     else:
         sys.exit("Exit program")
     
+def print_generate_reports_page():
+    page = "Reports Page"
+    description = "This generates the report for a certain curve"
+    options = [f'Curve {record[0]}, {record[1]}, date {(record[2][:10])}' for record in unique_curves(["curve", "thread", "date"])]
+    ui.print_page(page, description, options, "Exit")
+    selection = ui.page_selection(len(options))
+    if selection:
+        pass
+    else:
+        sys.exit("Exit program")
+
+
 def reports_page():
     selection = print_insert_main_page()
     if selection == 1:
-        pass
+        print_generate_reports_page()
     elif selection == 2:
         print_make_predictions_page()
-    elif selection == 3:
-        pass
     else:
         sys.exit("Exit program")
 
