95e509b8549f1cf377f23bf8c497260bbb46393b
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-13 22:47:40 -0500

    fix: Change raw data figure name

--- graphs/rawgraph.py
+++ graphs/rawgraph.py
@@ -8,7 +8,7 @@ from typing import Dict
 def raw_data_graph(record: Dict):
     data = read_catdata(record["raw_file"])
     graph_data(data, ["Raw Data Curve: %d" % record["curve"] , "Position $[km]$", "Raw Disp $[\mu m]$"]) #type: ignore
-    fig_name = "data/figures/curve{}_{}_{}.png".format(record['curve'], record["thread"], record["date"][:10])
+    fig_name = "data/figures/raw_curve{}_{}_{}.png".format(record['curve'], record["thread"], record["date"][:10])
     plt.savefig(fig_name, format="png")
 
 def main():
--- reports/pdfreport.py
+++ reports/pdfreport.py
@@ -23,14 +23,14 @@ class PDF(FPDF):
         self.cell(0,10, "Reperfilado: {}".format(reprofiling), 0,2,'L')
 
     def get_raw_data_graph(self, curve_record):
-        graph_dir = "data/figures/curve{}_{}_{}.png".format(curve_record['curve'], curve_record["thread"], curve_record["date"][:10])
+        graph_dir = "data/figures/raw_curve{}_{}_{}.png".format(curve_record['curve'], curve_record["thread"], curve_record["date"][:10])
         if not os.path.exists(graph_dir):
             raw_data_graph(curve_record)
         return graph_dir
 
     def print_raw_graph(self, curve_record):
         raw_image = self.get_raw_data_graph(curve_record)
-        self.image(raw_image, w=190, h=80)
+        self.image(raw_image, w=190, h=80, type='PNG')
 
     def graphs(self, curve_record):
         self.set_font('Arial','B',12)
