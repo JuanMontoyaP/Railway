ad48d97fd334508c25057ed0932796a1cd03adae
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-13 22:38:09 -0500

    Add: The raw image to the report

--- reports/pdfreport.py
+++ reports/pdfreport.py
@@ -1,6 +1,10 @@
+from graphs.graph import graph_data
 from graphs.rawgraph import raw_data_graph
 
+import os
+
 from typing import List
+
 from fpdf import FPDF
 
 class PDF(FPDF):
@@ -19,11 +23,18 @@ class PDF(FPDF):
         self.cell(0,10, "Reperfilado: {}".format(reprofiling), 0,2,'L')
 
     def get_raw_data_graph(self, curve_record):
-        raw_data_graph(curve_record)
+        graph_dir = "data/figures/curve{}_{}_{}.png".format(curve_record['curve'], curve_record["thread"], curve_record["date"][:10])
+        if not os.path.exists(graph_dir):
+            raw_data_graph(curve_record)
+        return graph_dir
+
+    def print_raw_graph(self, curve_record):
+        raw_image = self.get_raw_data_graph(curve_record)
+        self.image(raw_image, w=190, h=80)
 
     def graphs(self, curve_record):
         self.set_font('Arial','B',12)
         self.cell(0,10, "2. Gráficos", 0,2,'L')
         self.cell(0,10, "2.1 Raw data", 0,2,'L')
-        self.get_raw_data_graph(curve_record)
+        self.print_raw_graph(curve_record)
         
